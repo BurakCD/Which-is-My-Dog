ad6bff92ce124553c14a7d1c9a421c29d46495c5
BurakCD <50319976+BurakCD@users.noreply.github.com>
2022-07-14 21:17:46 +0300

    açılış sayfasına progress bar eklendi, ana sayfayı oluşturacak activity eklendi, expandable list view eklendi, child ve parent görünümleri açıldı

--- app/src/main/java/com/pixselect/whichismydog/MainActivity.java
+++ app/src/main/java/com/pixselect/whichismydog/MainActivity.java
@@ -2,13 +2,39 @@ package com.pixselect.whichismydog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+
+import com.pixselect.whichismydog.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private int progressStart = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        new CountDownTimer(5100,100){
+
+            @Override
+            public void onTick(long l) {
+                binding.progressBar.setProgress(progressStart += 2);
+                binding.progressBar.setMax(100);
+                //her 100 milisaniyede yüzde 2 = 1 saniyede yüzde 20 gönlüm el vermedi
+                //biterken progress bar dolmadığı için süreyi 5100 ms ye uzattım
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                finish();
+            }
+        }.start();
     }
+
 }
